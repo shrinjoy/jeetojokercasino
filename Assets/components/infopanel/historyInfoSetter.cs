17e9cabcb344c05df905cbb256119bcaf8b15872
Noiz <shrinjoybiswas0@gmail.com>
2023-05-03 14:39:08 +0530

    Update historyInfoSetter.cs

--- Assets/components/infopanel/historyInfoSetter.cs
+++ Assets/components/infopanel/historyInfoSetter.cs
@@ -15,33 +15,40 @@ public class historyInfoSetter : MonoBehaviour
     public TMPro.TMP_Text end_point_txt;
     public TMPro.TMP_Text claimtime;
     //
-    public void setdata(string sno,string barcode,string play,string win,string result,string status,string stpoint,string claimdatetime)
+    public void setdata(string sno, string barcode, string play, string win, string result, string status, string stpoint, string claimdatetime)
     {
-        serialnumber.text = sno;    
+        serialnumber.text = sno;
         gameid.text = barcode;
         playtext.text = play;
         wintext.text = win;
 
         resulttext.text = result;
-        if(status.ToLower() == "print" )
+        if (status.ToLower() == "print")
         {
             status = "N/W";
         }
         statustxt.text = status;
         int winpoint = 0;
-        if (win==null || win.ToString().Trim().Length==0 ) {
+        if (win == null || win.ToString().Trim().Length == 0) {
 
             winpoint = 0;
         }
         else
         {
-             winpoint = Convert.ToInt32(win);
+            winpoint = Convert.ToInt32(win);
 
         }
         int start = Convert.ToInt32(stpoint);
         int totalplayed = (Convert.ToInt32(play));
-        int end = start-totalplayed+winpoint;
-        claimtime.text = DateTime.Parse(claimdatetime).ToString("ddMMM|hh:mmtt");
+        int end = start - totalplayed + winpoint;
+        if (claimdatetime == null || claimdatetime.ToString().Trim().Length == 0)
+        {
+            claimtime.text = " ";
+        }
+        else
+        {
+            claimtime.text = DateTime.Parse(claimdatetime).ToString("ddMMM|hh:mmtt");
+        }
         start_point_txt.text = stpoint;
         end_point_txt.text =end.ToString();
 
